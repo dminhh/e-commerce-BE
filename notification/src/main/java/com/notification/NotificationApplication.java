4d544c17f0c8a991307a831f7b0f1b7f9bf7b328
hoducminh <ducminhk42a1@gmail.com>
2025-11-12 09:24:39 +0700

    [dminh] update eureka

--- notification/src/main/java/com/notification/NotificationApplication.java
+++ notification/src/main/java/com/notification/NotificationApplication.java
@@ -2,6 +2,7 @@ package com.notification;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
@@ -9,6 +10,7 @@ import org.springframework.context.annotation.ComponentScan;
         "com.common.*",
         "com.notification.*"
 })
+@EnableDiscoveryClient
 public class NotificationApplication {
 
     public static void main(String[] args) {
--- product/src/main/java/com/prod/ProductApplication.java
+++ product/src/main/java/com/prod/ProductApplication.java
@@ -2,6 +2,7 @@ package com.prod;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
@@ -9,6 +10,7 @@ import org.springframework.context.annotation.ComponentScan;
         "com.common.*",
         "com.prod.*"
 })
+@EnableDiscoveryClient
 public class ProductApplication {
     public static void main(String[] args) {
         SpringApplication.run(ProductApplication.class, args);
